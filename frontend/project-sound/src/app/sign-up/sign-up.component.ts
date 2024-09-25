0d7956661fcf154f8a653039fd77fdefa7fb6fed
Jram2001 <jayaramasankaramoorthi@gmail.com>
2024-09-25 12:06:06 -0500

    implemented routing and compiled login form ui

--- frontend/project-sound/src/app/sign-up/sign-up.component.ts
+++ frontend/project-sound/src/app/sign-up/sign-up.component.ts
@@ -8,14 +8,13 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class SignUpComponent {
   isVerified:boolean = false
-  signUp!: FormGroup<{ email: FormControl<string | null>; username: FormControl<string | null>; password: FormControl<string | null>; confirmpassword: FormControl<string | null>; otp: FormControl<string | null>; }>;
+  signUp!: FormGroup<{ email: FormControl<string | null>; username: FormControl<string | null>; password: FormControl<string | null>; confirmpassword: FormControl<string | null> }>;
   ngOnInit(){
     this.signUp = new FormGroup({
       email: new FormControl('' , Validators.required),
       username: new FormControl('' ,Validators.required),
       password: new FormControl('' , Validators.required),
       confirmpassword: new FormControl('' ,Validators.required),
-      otp: new FormControl('' ,Validators.required)
     })
   }
   isLoading:boolean = false;
